854cd2e2e900f6893499a706d84f33b04cda9bbe
Tom Yarrow <tyarrow1@gmail.com>
2018-09-16 11:30:13 -0400

    merged backend and frontend

--- Extension/activate.js
+++ Extension/activate.js
@@ -3,18 +3,37 @@ var currentUrl;
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
 function(tabs){
     	currentUrl = tabs[0].url;
-	//sendURLfunction();
-	receiveDataFunction();
+
+
+
+	postURLfunction();
+
 	}
 );
-function sendURLfunction() {
+function postURLfunction(){
 	//run the (url-using) function here
 	chrome.extension.getBackgroundPage().console.log(currentUrl);
 
 	document.getElementById("Title1").innerHTML = currentUrl;
   chrome.extension.getBackgroundPage().console.log("localhost:5000?url=" + currentUrl);
-  //fetch("localhost:5000/?url=" + currentUrl);
-  var xhttp = new XMLHttpRequest();
+  fetch("http://localhost:5000/?url=" + currentUrl).then(function(response){
+    return response.json;
+  }).then(function(json){
+    recieveDataFunction(json);
+  });
+  function receiveDataFunction(response) {
+      var data = JSON.parse(response);
+      //var data = {num:6,s:["Test1","Test2","Test3","Test4","Test5","Test6"],l:["https://www.w3schools.com","https://www.w3schools.com","https://www.w3schools.com"]};
+      document.getElementById("theScore").innerHTML = data.num;
+      for(i = 0;i < Math.min(data.s.length,10);i++) {
+  	document.getElementById("s"+i).innerHTML = data.s[i];
+      }
+      for(i = 0;i < Math.min(data.l.length,5);i++) {
+  	document.getElementById("s"+(2*i+1)).href = data.l[i];
+  	chrome.extension.getBackgroundPage().console.log(2*i+1);
+      }
+  }
+  /*var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      chrome.extension.getBackgroundPage().console.log(this.responseText);
@@ -23,21 +42,36 @@ function sendURLfunction() {
   xhttp.open("GET", "http://127.0.0.1:5000/?url=" + encodeURIComponent(currentUrl), true);
   xhttp.send();
 
-  xhttp.onreadystatechange = function() {
-  if (this.readyState == 4 && this.status == 200) {
-    recieveDataFunction(xhttp.responseText);
-  }
-};
+    let result = xhttp.response();*/
+
+    /*$.get("http://127.0.0.1:5000/?url=" + encodeURIComponent(currentUrl), function(response) {
+        console.log(response);
+    });*/
+
+/*    let response = await axios.get("http://127.0.0.1:5000/?url=" + encodeURIComponent(currentUrl));
+    console.log(response);*/
 }
-function receiveDataFunction(response) {
-    //var data = JSON.parse(response);
-    var data = {num:6,s:["Test1","Test2","Test3","Test4","Test5","Test6"],l:["https://www.w3schools.com","https://www.w3schools.com","https://www.w3schools.com"]};
-    document.getElementById("theScore").innerHTML = data.num;
-    for(i = 0;i < Math.min(data.s.length,10);i++) {
-	document.getElementById("s"+i).innerHTML = data.s[i];
+
+/*
+  var google = require('google');
+  chrome.extension.getBackgroundPage().console.log(google);
+  google.resultsPerPage = 25;
+  var nextCounter = 0;
+
+  google("viraat das", function (err, res){
+    chrome.extension.getBackgroundPage().console.log("hi");
+    if (err) console.error(err);
+
+    for (var i = 0; i < res.links.length; ++i) {
+      var link = res.links[i];
+      chrome.extension.getBackgroundPage().console.log(link.title + ' - ' + link.href);
+      chrome.extension.getBackgroundPage().console.log(link.description + "\n");
+
+
+
     }
-    for(i = 0;i < Math.min(data.l.length,5);i++) {
-	document.getElementById("s"+(2*i+1)).href = data.l[i];
-	chrome.extension.getBackgroundPage().console.log(2*i+1);
-    }
-}
+
+    if (nextCounter < 4) {
+      nextCounter += 1;
+      if (res.next) res.next();
+    }*/
